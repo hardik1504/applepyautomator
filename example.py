96bca1c9844ae476e664233084b8a3e13e9f0380
Hardik Sharma <65143877+hardik1504@users.noreply.github.com>
2020-05-11 12:25:42 +0530

    Update example.py

--- example.py
+++ example.py
@@ -9,29 +9,29 @@ password = "password"
 
 # example twitter login
 # Launch Google Chrome
-automator.launch_app("Google Chrome")   
+automator.launch_app("Google Chrome")
 # Type twitter URL
-automator.type_keystroke("https://twitter.com/login")   
+automator.type_keystroke("https://twitter.com/login")
 # Press ENTER
-automator.press_key(COMMON_KEYS.ENTER)   
+automator.press_key(COMMON_KEYS.ENTER)
 # Wait for 2 seconds and then type email
-automator.type_keystroke(email, delay=2)    
+automator.type_keystroke(email, delay=2)
 # Press TAB
 automator.press_key(COMMON_KEYS.TAB)
 # Type Password
-automator.type_keystroke(password)   
+automator.type_keystroke(password)
 # Press ENTER
-automator.press_key(COMMON_KEYS.ENTER)    
+automator.press_key(COMMON_KEYS.ENTER)
 
 # open inspect element in google chrome and copy html code
 # Press F12 to open developer options
-automator.press_key(FUNCT_KEYS.F12)  
+automator.press_key(FUNCT_KEYS.F12)
 # Press F2
-automator.press_key(FUNCT_KEYS.F2)
+automator.press_key(FUNCT_KEYS.F2, delay=2)
 # Select all
-automator.press_key(SHORTCUTS.SELECT_ALL)
+automator.press_key(SHORTCUTS.SELECT_ALL, delay=1)
 # copy
-automator.press_key(SHORTCUTS.COPY)
+automator.press_key(SHORTCUTS.COPY, delay=1)
 
 # quit Google Chrome
 automator.quit_app("Google Chrome", delay=2)
